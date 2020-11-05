2b337216c85dff11b474eb4089de7dd5446cf45a
Ughur Guliyev <ughurguliyev@MacBooks-MacBook-Air.local>
2020-11-05 19:21:53 +0400

    third commit

---
+++ blog/post/models/article.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+from .category import Category
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=40, verbose_name="Title")
+    body = models.TextField(verbose_name="Body of article")
+    post_image = models.ImageField(null=True)
+    pub_date = models.DateTimeField(auto_now=timezone.now())
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.title
